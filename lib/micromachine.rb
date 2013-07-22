b416103f61b10cff1643343b03c69b6172aee472
hakanensari <hakan.ensari@papercavalier.com>
2013-07-22 19:00:15 +0100

    Parenthetical gardening

--- lib/micromachine.rb
+++ lib/micromachine.rb
@@ -4,13 +4,13 @@ class MicroMachine
   attr :transitions_for
   attr :state
 
-  def initialize initial_state
+  def initialize(initial_state)
     @state = initial_state
     @transitions_for = Hash.new
     @callbacks = Hash.new { |hash, key| hash[key] = [] }
   end
 
-  def on key, &block
+  def on(key, &block)
     @callbacks[key] << block
   end
 
@@ -18,8 +18,8 @@ class MicroMachine
     transitions_for[event] = transitions
   end
 
-  def trigger event
     if trigger?(event)
+  def trigger(event)
       @state = transitions_for[event][@state]
       callbacks = @callbacks[@state] + @callbacks[:any]
       callbacks.each { |callback| callback.call }
