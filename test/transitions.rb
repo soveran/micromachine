1a4bb71a9aaac405d8630e66ec725387755f9c9d
y8 <y8@ya.ru>
2015-11-07 12:34:25 +0100

    Cover `previous_state` changes with tests

--- test/transitions.rb
+++ test/transitions.rb
@@ -12,6 +12,7 @@ end
 
 test "defines initial state" do |machine|
   assert_equal :pending, machine.state
+  assert_equal nil, machine.previous_state
 end
 
 test "raises an error if an invalid event is triggered" do |machine|
@@ -24,26 +25,32 @@ test "preserves the state if transition is not possible" do |machine|
   assert !machine.trigger?(:reset)
   assert !machine.trigger(:reset)
   assert_equal :pending, machine.state
+  assert_equal nil, machine.previous_state
 end
 
 test "changes the state if transition is possible" do |machine|
   assert machine.trigger?(:confirm)
   assert machine.trigger(:confirm)
   assert_equal :confirmed, machine.state
+  assert_equal :pending, machine.previous_state
 end
 
 test "discerns multiple transitions" do |machine|
   machine.trigger(:confirm)
   assert_equal :confirmed, machine.state
+  assert_equal :pending, machine.previous_state
 
   machine.trigger(:reset)
   assert_equal :pending, machine.state
+  assert_equal :confirmed, machine.previous_state
 
   machine.trigger(:ignore)
   assert_equal :ignored, machine.state
+  assert_equal :pending, machine.previous_state
 
   machine.trigger(:reset)
   assert_equal :pending, machine.state
+  assert_equal :ignored, machine.previous_state
 end
 
 test "raises an error if event is triggered from/to a non complatible state" do |machine|
